e4ed5877dd6d18003ace9e98586a2f2e614f64da
ArmandoCQ <58895266+ArmandoCQ@users.noreply.github.com>
2019-12-14 21:47:51 -0600

    Update run.py

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ app = create_app()
 if __name__ == '__main__':
 #    app.run(debug=True)
     #To run in local network
-    app.run(host='192.168.100.16',port=5005)
+    app.run(host=os.environ.get('IP_ADDRESS'),port=os.environ.get('port'))
     
